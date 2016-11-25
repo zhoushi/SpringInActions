05f283a0b24d9a0364d7d012ec042f54d747b07c
zhoushi <1334599897@qq.com>
2016-11-25 11:54:21 +0800

    spring整合redis并使用redis

--- SpringRabbit/src/main/java/com/spring/consumer/ConsumerMain.java
+++ SpringRabbit/src/main/java/com/spring/consumer/ConsumerMain.java
@@ -5,6 +5,7 @@ import com.spring.dao.SpittleAlertHandler;
 import com.spring.domain.Demo;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,25 +18,33 @@ public class ConsumerMain {
     /**
      * 常规的得到消息
      */
-//    //得到bean工厂
-//    ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//    //得到bean
-//    RabbitTemplate  template = (RabbitTemplate) context.getBean("rabbitTemplate2");
-//    //收到信息
-//    Message message =template.receive();
-//    System.out.println(message.toString());
-//    //信息转换
-//    Demo demo = (Demo) template.receiveAndConvert("spittle.alerts");
+    //得到bean工厂
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    //得到bean
+    RabbitTemplate  template = (RabbitTemplate) context.getBean("rabbitTemplate2");
+    //收到信息
+    Message message =template.receive();
+    System.out.println(message.toString());
+    MessageProperties demo = message.getMessageProperties();
+
+
+      ;
+    //信息转换
+//      message.getBodyContentAsString();
+//      for (int  i=0;i<demo.length;i++){
+//          System.out.println(demo[i]);
+//      }
+
 //    System.out.println(demo.toString());
 
-//    System.out.println(message.toString());
-//    SpittleAlertHandler spittleAlertHandler;
+    System.out.println(message.toString());
+    SpittleAlertHandler spittleAlertHandler;
     /**
      * 消息驱动
      */
-    ApplicationContext context = new ClassPathXmlApplicationContext("amqp-shared.xml");
-    DemoAlertHandler demoAlertHandler;
-    System.in.read();
+//    ApplicationContext context = new ClassPathXmlApplicationContext("amqp-shared.xml");
+//    DemoAlertHandler demoAlertHandler;
+//    System.in.read();
   }
 
 }
---
+++ SpringRedis/src/main/java/com/spring/Redis/RedisConfig.java
@@ -0,0 +1,53 @@
+package com.spring.Redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Created by Administrator on 2016/11/25.
+ */
+@Configuration
+public class RedisConfig {
+    /**
+     * 注入redis连接工厂bean，让spring管理
+     * @return
+     * 默认情况下会向localhost上的6379端口创建连接
+     */
+    @Bean
+    public RedisConnectionFactory redisCF(){
+        JedisConnectionFactory cf = new JedisConnectionFactory();
+        cf.setHostName("127.0.0.1");
+        cf.setPort(6379);
+        return cf;
+    }
+
+    /**
+     * 注入RedisTemplate Bean
+     * RedisTemplate可以极大地简化Redis数据访问，能够让我们持久化各种类型的key和value
+     * StringRedisTemplate扩展了RedisTemplate只关注String类型
+     * @param cf
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String,Product> redisTemplate(RedisConnectionFactory cf){
+        RedisTemplate<String,Product> redis = new RedisTemplate<>();
+        redis.setConnectionFactory(cf);
+        redis.setKeySerializer(new StringRedisSerializer());
+        redis.setValueSerializer(new Jackson2JsonRedisSerializer<Product>(Product.class));
+        return redis;
+    }
+
+    @Bean
+    public RedisTemplate<String,String> redisTemplate1(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String,String> redis = new RedisTemplate<String, String>();
+        redis.setConnectionFactory(connectionFactory);
+        redis.setKeySerializer(new StringRedisSerializer());
+        redis.setValueSerializer(new StringRedisSerializer());
+        return redis;
+    }
+}
